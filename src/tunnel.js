5369b9fa27675157a67087769b802ddfc376483f
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2023-07-04 13:23:01 -0300

    Added proxy feature

--- src/tunnel.js
+++ src/tunnel.js
@@ -2,35 +2,55 @@ require('colors');
 const axios = require('axios');
 const debug = require('debug')('mocko:tunnel:tunnel');
 
+const REMOVE_HEADERS = new Set();
+REMOVE_HEADERS.add('transfer-encoding');
+REMOVE_HEADERS.add('accept-encoding');
+REMOVE_HEADERS.add('content-length');
+REMOVE_HEADERS.add('connection');
+
 let lastId = '0';
 
 async function tunnel(port, token) {
     debug('connecting to stream');
-    const { data } = await axios.post(`https://dev-mocko.free.mockoapp.net/tunnels/${token}/subscribers`)
+    const { data } = await axios.post(`https://dev-mocko.free.mockoapp.net/tunnels/${token}/subscribers`)  // TODO implement
         .catch(exitWithError);
 
     lastId = data.start;
-    fetchPage(data.subscriberId);
+    fetchPage(data.subscriberId, port);
     console.log(`${'✔'.green} Forwarding requests from ${data.url} to local port ${port}`);
 }
 
-async function fetchPage(subscriberId) {
+async function fetchPage(subscriberId, port) {
     debug('fetching request page');
     const { data } = await axios.get(`https://api.codetunnel.net/stream/v1/subscribers/${subscriberId}/messages?wait=true&after=${lastId}`)
         .catch(exitWithError);
 
     data.forEach(([id, req]) => {
         lastId = id;
-        sendRequest(subscriberId, id, req);
+        sendRequest(subscriberId, id, req, port);
     });
 
-    setTimeout(() => fetchPage(subscriberId), 100);
+    setTimeout(() => fetchPage(subscriberId, port), 100);
 }
 
-async function sendRequest(token, id, req) {
+async function sendRequest(token, id, message, port) {
     debug('sending request');
-    // TODO request
-    const data = req;
+    const req = JSON.parse(message);
+
+    console.log(`${req.method.toUpperCase()} ${req.path}`);
+    const res = await axios({
+        method: req.method,
+        url: `http://localhost:${port}${req.path}`,
+        data: Buffer.from(req.body, 'base64'),
+        headers: req.headers,
+        validateStatus: () => true,
+        responseType: 'arraybuffer',
+    });
+    const data = JSON.stringify({
+        status: res.status,
+        headers: sanitizeHeaders(res.headers),
+        body: res.data.toString('base64'),
+    });
 
     debug('publishing response');
     try {
@@ -51,6 +71,12 @@ function exitWithError(error) {
 
     console.error(message);
     process.exit(1);
+}
+
+function sanitizeHeaders(headers) {
+    const entries = Object.entries(headers)
+        .filter(([k]) => !REMOVE_HEADERS.has(k));
+    return Object.fromEntries(entries);
 }
 
 module.exports = { tunnel };
