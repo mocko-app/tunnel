9c48b480d8b0c7d1abf0b84fbc26fda5a80fb39f
Gabriel Pinheiro <gabrielctpinheiro@gmail.com>
2023-07-04 23:04:42 -0300

    Extracting utils.js

--- src/tunnel.js
+++ src/tunnel.js
@@ -1,40 +1,37 @@
 require('colors');
 const axios = require('axios');
+const { exitWithError, sleep, toTunnelResponse, toTunnelError, noop } = require('./utils');
 const debug = require('debug')('mocko:tunnel:tunnel');
 
-const REMOVE_HEADERS = new Set();
-REMOVE_HEADERS.add('transfer-encoding');
-REMOVE_HEADERS.add('accept-encoding');
-REMOVE_HEADERS.add('content-length');
-REMOVE_HEADERS.add('connection');
-
-let lastId = '0';
+let page = '0';
 
 async function tunnel(port, token) {
     debug('connecting to stream');
     const { data } = await axios.post(`https://dev-mocko.free.mockoapp.net/tunnels/${token}/subscribers`)  // TODO implement
-        .catch(exitWithError);
+        .catch(exitWithError('Failed to connect to Mocko: '));
 
-    lastId = data.start;
-    fetchPage(data.subscriberId, port);
+    page = data.start;
     console.log(`${'✔'.green} Forwarding requests from ${data.url} to local port ${port}`);
+
+    while(page) {
+        await fetchPage(data.subscriberId, port);
+        await sleep(100);
+    }
 }
 
 async function fetchPage(subscriberId, port) {
     debug('fetching request page');
-    const { data } = await axios.get(`https://api.codetunnel.net/stream/v1/subscribers/${subscriberId}/messages?wait=true&after=${lastId}`)
-        .catch(exitWithError);
+    const { data } = await axios.get(`https://api.codetunnel.net/stream/v1/subscribers/${subscriberId}/messages?wait=true&after=${page}`)
+        .catch(exitWithError('Error: '));
 
     data.forEach(([id, req]) => {
-        lastId = id;
+        page = id;
         sendRequest(subscriberId, id, req, port);
     });
-
-    setTimeout(() => fetchPage(subscriberId, port), 100);
 }
 
 async function sendRequest(token, id, message, port) {
-    debug('sending request');
+    debug(`sending request`);
     const req = JSON.parse(message);
 
     console.log(`${req.method.toUpperCase()} ${req.path}`);
@@ -46,52 +43,20 @@ async function sendRequest(token, id, message, port) {
         validateStatus: () => true,
         responseType: 'arraybuffer',
     });
-    const data = JSON.stringify({
-        status: res.status,
-        headers: sanitizeHeaders(res.headers),
-        body: res.data.toString('base64'),
-    });
+    const data = toTunnelResponse(res.status, res.headers, res.data);
 
-    debug('publishing response');
+    debug(`publishing ${res.status} response of ${req.method} ${req.path}`);
     try {
         if(data.length > 8192) {
             throw new Error('Response is too large to be tunneled back');
         }
         await axios.post(`https://api.codetunnel.net/stream-response/v1/subscribers/${token}/responses`, { id, data });
     } catch(e) {
-        const badGateway = JSON.stringify({
-            status: 502,
-            headers: { 'Content-Type': 'application/json' },
-            body: Buffer.from(JSON.stringify({
-                statusCode: 502,
-                error: 'Bad Gateway',
-                message: 'Failed to tunnel response back, check the mocko-tunnel CLI logs for more information',
-            })).toString('base64'),
-        });
-
+        const badGateway = toTunnelError('Failed to tunnel response back, check the mocko-tunnel CLI logs for more information')
         console.warn(`Failed to publish response: ${e?.response?.data?.message || e?.message || e}`);
         await axios.post(`https://api.codetunnel.net/stream-response/v1/subscribers/${token}/responses`, { id, data: badGateway })
-            .catch(() => {});
+            .catch(noop);
     }
-}
-
-function exitWithError(error) {
-    let message = 'Error: ' + (error?.response?.data?.message ?? error?.message ?? error);
-    if(error?.response?.data?.statusCode === 404) {
-        message = 'Invalid or expired token, you can find it in your project settings: https://app.mocko.dev/tunnels';
-    }
-    if(error?.response?.data?.statusCode === 409) {
-        message = 'Your token expired, get a new one in your project settings: https://app.mocko.dev/tunnels';
-    }
-
-    console.error(message);
-    process.exit(1);
-}
-
-function sanitizeHeaders(headers) {
-    const entries = Object.entries(headers)
-        .filter(([k]) => !REMOVE_HEADERS.has(k));
-    return Object.fromEntries(entries);
 }
 
 module.exports = { tunnel };
---
+++ src/utils.js
@@ -0,0 +1,50 @@
+const REMOVE_HEADERS = new Set();
+REMOVE_HEADERS.add('transfer-encoding');
+REMOVE_HEADERS.add('accept-encoding');
+REMOVE_HEADERS.add('content-length');
+REMOVE_HEADERS.add('connection');
+
+const noop = () => { };
+const sleep = t => new Promise(r => setTimeout(r, t));
+
+const exitWithError = (tag) => (error) => {
+    let message = tag + (error?.response?.data?.message ?? error?.message ?? error);
+    if(error?.response?.data?.statusCode === 404) {
+        message = 'Invalid or expired token, you can find it in your project settings: https://app.mocko.dev/tunnels';
+    }
+    if(error?.response?.data?.statusCode === 409) {
+        message = 'Your token expired, get a new one in your project settings: https://app.mocko.dev/tunnels';
+    }
+
+    console.error(message);
+    process.exit(1);
+};
+
+function toTunnelResponse(status, headers, data) {
+    const dataBuffer = typeof data === 'string' ? Buffer.from(data) : data;
+    const body = dataBuffer.toString('base64');
+    console.log(body);
+
+    return JSON.stringify({
+        status, body,
+        headers: sanitizeHeaders(headers),
+    });
+}
+
+function toTunnelError(message) {
+    const headers = { 'Content-Type': 'application/json' };
+    const body = JSON.stringify({
+        statusCode: 502,
+        error: 'Bad Gateway',
+        message,
+    });
+    return toTunnelResponse(502, headers, body);
+}
+
+function sanitizeHeaders(headers) {
+    const entries = Object.entries(headers)
+        .filter(([k]) => !REMOVE_HEADERS.has(k));
+    return Object.fromEntries(entries);
+}
+
+module.exports = { toTunnelResponse, toTunnelError, exitWithError, sleep, noop };
